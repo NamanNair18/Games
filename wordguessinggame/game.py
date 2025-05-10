ae2ae9de5781d003dd4f72a3ffd9831ecace5e83
NamanNair18 <namannair18@gmail.com>
2025-05-10 12:36:06 +0530

    Update game.py

--- wordguessinggame/game.py
+++ wordguessinggame/game.py
@@ -7,5 +7,18 @@ word = random.choice(word_bank)#used to select a random word from the world_bank
 guessedword = ['_'] * len(word)
 attempts = 10
 while attempts > 0:
-    print('/n Current Word: ' + ' ' .Join(guessedword))
+    print('\n Current Word: ' + ' ' .Join(guessedword))
+    guess = input('Guess the Letter: ')
 
+    if guess in word:
+        for i in range (len(word)):
+            if word[i] == guess:
+                guessedword[i]== guess
+                print('Great Guess!!')
+    else:
+        attempts -= 1
+        print('Wrong guess! Attempts Left: ' + str(attempts))
+
+    if '_' not in guessedword:
+        print('\n Congratulations !! You guessed the Word: ' + word)
+    break
