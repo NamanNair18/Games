0349e6a57524f33942e2b7bb2e252f58d09ed547
NamanNair18 <namannair18@gmail.com>
2025-05-17 21:05:19 +0530

    Update game.py

--- rock,paper,scissor,lizard,spock/game.py
+++ rock,paper,scissor,lizard,spock/game.py
@@ -1,9 +1,9 @@
 win_lose_pairs = [('Scissors','Paper'),
-                  ('Paper','Rock'),
-                  ('Rock','Lizard'),
-                  ('Lizard','Spock'),
-                  ('Spock','Scissors'),
+                  ('Paper'   ,'Rock'),
+                  ('Rock'    ,'Lizard'),
+                  ('Lizard'  ,'Spock'),
+                  ('Spock'   ,'Scissors'),
                   ('Scissors','Lizard'),
-                  ('Lizard','Paper'),
-                  ('Spock','Rock'),
-                  ('Rock','Scissors'),]
+                  ('Lizard'  ,'Paper'),
+                  ('Spock'   ,'Rock'),
+                  ('Rock'    ,'Scissors'),]
