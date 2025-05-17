0c51e6e3125a4e1433b5ac86cb6194a4c2ff09c7
NamanNair18 <namannair18@gmail.com>
2025-05-17 21:26:00 +0530

    Update game.py

--- rock,paper,scissor,lizard,spock/game.py
+++ rock,paper,scissor,lizard,spock/game.py
@@ -17,7 +17,14 @@ computer_choice = random.choice(choices)
 print(' Please choose one of the option below: ')
 print(' -> 0 for Rock\n -> 1 for Paper\n -> 2 for Scissors\n -> 3 for Lizard -> 4 for Spock')
 
-user_input = int(input(' Your choice (0 - 4):  '))
+while True:
+    try:
+        user_input = int(input(' Your choice (0 - 4):  '))
+        if user_input not in range(5):
+            raise ValueError
+        break
+    except ValueError:
+        print('Enter number only')
 
 human_choice = choices[user_input]
 
@@ -29,3 +36,13 @@ elif (human_choice, computer_choice) in win_lose_pairs:
 
 else:
     result = 'You Lost!'
+
+print('--------------------------------------')
+print(' > You Chose      : ',human_choice)
+print(' > Computer Chose : ', computer_choice)
+print(' > Result         : ', result)
+print('--------------------------------------')
+
+play_again = input('Play again (Y/N): ')
+if play_again.lower() == 'n':
+    break 
