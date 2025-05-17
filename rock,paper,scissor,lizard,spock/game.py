c8c449850b027ee883abdca452d7cffa5c0a57a0
NamanNair18 <namannair18@gmail.com>
2025-05-17 21:10:56 +0530

    Update game.py

--- rock,paper,scissor,lizard,spock/game.py
+++ rock,paper,scissor,lizard,spock/game.py
@@ -13,3 +13,6 @@ win_lose_pairs = [('Scissors','Paper'),
 choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 
 computer_choice = random.choice(choices)
+
+print(' Please choose one of the option below: ')
+print(' -> 0 for Rock\n -> 1 for Paper\n -> 2 for Scissors\n -> 3 for Lizard -> 4 for Spock')
