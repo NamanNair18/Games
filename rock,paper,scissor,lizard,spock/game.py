912584147b993ddfd73da63e11e744c83bf4ab7a
NamanNair18 <namannair18@gmail.com>
2025-05-17 21:31:19 +0530

    Update game.py

--- rock,paper,scissor,lizard,spock/game.py
+++ rock,paper,scissor,lizard,spock/game.py
@@ -12,37 +12,36 @@ win_lose_pairs = [('Scissors','Paper'),
 
 choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 
-computer_choice = random.choice(choices)
-
-print(' Please choose one of the option below: ')
-print(' -> 0 for Rock\n -> 1 for Paper\n -> 2 for Scissors\n -> 3 for Lizard -> 4 for Spock')
-
 while True:
-    try:
-        user_input = int(input(' Your choice (0 - 4):  '))
-        if user_input not in range(5):
-            raise ValueError
+    computer_choice = random.choice(choices)
+
+    print(' Please choose one of the option below: ')
+    print(' -> 0 for Rock\n -> 1 for Paper\n -> 2 for Scissors\n -> 3 for Lizard -> 4 for Spock')
+
+    while True:
+        try:
+            user_input = int(input(' Your choice (0 - 4):  '))
+            if user_input not in range(5):
+                raise ValueError
+            break
+        except ValueError:
+            print('Enter number (0 - 4) ')
+
+    human_choice = choices[user_input]
+
+    if computer_choice == human_choice:
+        result = 'Draw!'
+    elif (human_choice, computer_choice) in win_lose_pairs:
+        result = 'You won!'
+    else:
+        result = 'You Lost!'
+
+    print('--------------------------------------')
+    print(' > You Chose      : ', human_choice)
+    print(' > Computer Chose : ', computer_choice)
+    print(' > Result         : ', result)
+    print('--------------------------------------')
+
+    play_again = input('Play again (Y/N): ')
+    if play_again.lower() == 'n':
         break
-    except ValueError:
-        print('Enter number (0 - 4) ')
-
-human_choice = choices[user_input]
-
-if computer_choice == human_choice:
-    result = 'Draw!'
-
-elif (human_choice, computer_choice) in win_lose_pairs:
-    result = 'You won!'
-
-else:
-    result = 'You Lost!'
-
-print('--------------------------------------')
-print(' > You Chose      : ',human_choice)
-print(' > Computer Chose : ', computer_choice)
-print(' > Result         : ', result)
-print('--------------------------------------')
-
-play_again = input('Play again (Y/N): ')
-if play_again.lower() == 'n':
-    break 
